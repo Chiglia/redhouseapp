3b5008326acf223e71f204c0c6a4fe684fa9a5ad
Davide Chigliaro <redhouseverified@gmail.com>
2019-04-24 01:34:49 +0200

    database

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const cool = require('cool-ascii-faces')
+const seconda = require('cool-ascii-faces')
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+var mysql      = require('mysql');
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
@@ -11,7 +12,33 @@ express()
   .get('/progetti', (req, res) => res.render('pages/progetti'))
   .get('/chisiamo', (req, res) => res.render('pages/chisiamo'))
   .get('/valori', (req, res) => res.render('pages/valori'))
-  .get('/cool', (req, res) => res.send(cool()))  
+  .get('/seconda', (req, res) => res.send(seconda()))  
   .listen(PORT, () => console.log(`è qui: ${ PORT }`))
 
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'sampleDB'
+});
+var app = express();
 
+connection.connect(function(err){
+if(!err) {
+    console.log("Database is connected ... ");    
+} else {
+    console.log("Error connecting database ... ");    
+}
+});
+
+app.get("/seconda",function(req,res){
+connection.query('SELECT * from user LIMIT 2', function(err, rows, fields) {
+connection.end();
+  if (!err)
+    console.log('The solution is: ', rows);
+  else
+    console.log('Error while performing Query.');
+  });
+});
+
+app.listen(3000);
