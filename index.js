f8ae770e6fea12da2780c4981f5ef75cfea7061d
Davide Chigliaro <redhouseverified@gmail.com>
2019-04-23 20:22:09 +0200

    tutto

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+const cool = require('cool-ascii-faces')
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .get('/progetti', (req, res) => res.render('pages/progetti'))
+  .get('/chisiamo', (req, res) => res.render('pages/chisiamo'))
+  .get('/valori', (req, res) => res.render('pages/valori'))
+  .get('/cool', (req, res) => res.send(cool()))  
+  .listen(PORT, () => console.log(`è qui: ${ PORT }`))
+
+
 var mysql = require('mysql');
 var express = require('express');
 var app = express();
@@ -16,7 +33,7 @@ con.connect(function(err) {
     console.log("Connected!");
  }
     app.get('/',function(req,resp)){
-            con.query("SELECT *FROM sampleDB",function(error, rows, fields){
+            con.query("SELECT *FROM tabellabella",function(error, rows, fields){
             if (!!err){
   console.log("ERROR in the query");
 }else{
