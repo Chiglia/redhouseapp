1aa26b6b25c48907d5e91e1799b05ca0e8f2ca4d
Davide Chigliaro <redhouseverified@gmail.com>
2019-04-25 16:19:01 +0200

    HTML base

--- index.js
+++ index.js
@@ -20,9 +20,10 @@ const server = express()
 
 const io = socketIO(server);
 
-io.on('connection', (socket) => {
-  console.log('Client connected');
-  socket.on('disconnect', () => console.log('Client disconnected'));
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
 });
 
 setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
