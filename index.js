b26b683a3568eab9e7e1993c9b7973e579d6b2ed
Davide Chigliaro <redhouseverified@gmail.com>
2019-04-30 16:22:43 +0200

    orologio

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+    
+'use strict';
 
-express()
+const express = require('express');
+const socketIO = require('socket.io');
+const path = require('path');
+const PORT = process.env.PORT || 3000;
+
+const server = express()
+  
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .get('/progetti', (req, res) => res.render('pages/progetti'))
+  .get('/chisiamo', (req, res) => res.render('pages/chisiamo'))
+  .get('/valori', (req, res) => res.render('pages/valori'))
+  .listen(PORT, () => console.log(`è qui: ${ PORT }`));
+
+const io = socketIO(server);
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
+setInterval(() => io.emit('time', new Date().getHours()+2 + ":" + new Date().getMinutes() + ":" + new Date().getSeconds() + " GTM+0200 (UTC)"), 1000);
